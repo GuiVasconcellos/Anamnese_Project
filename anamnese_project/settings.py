bd9648d69c60589a97bb4ef1e69bfdecd13e5c57
GuiVasconcellos <guivasconcellosoliveira@gmail.com>
2026-02-26 18:23:44 -0300

    novo espaçamento

--- anamnese_project/settings.py
+++ anamnese_project/settings.py
@@ -68,7 +68,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'anamnese_project', 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
